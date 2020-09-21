bf433d376410d4820c99dacd55815af23040b4c1
aaadityaaaa <aaadityaaazz@gmail.com>
2020-09-21 23:02:44 +0530

    latest commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Aaditya Singh on 29/08/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String //body fo the message
+}
+
+
+
